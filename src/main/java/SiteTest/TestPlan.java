5367493a242a7e38e87e18cc911ff810a461c4f1
nlepykh <nazarlepykh@gmail.com>
2020-11-02 11:17:54 +0200

    First 5 cases.

---
+++ src/main/java/SiteTest/TestPlan.java
@@ -0,0 +1,31 @@
+package SiteTest;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TestPlan {
+    private static final WebDriver driver = new ChromeDriver();
+
+    @BeforeClass
+    public static void main(String[] args) {
+        // ChromeDriver location set up in Utils class
+        System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
+    }
+
+ @Test
+    public void submitForm(){
+        driver.get(Utils.BASE_URL2);
+        WebForm webForm = new WebForm(driver);
+        webForm.EnterSearchText("Selenium");
+        //webForm.pressSubmitButton();
+    }
+    @AfterClass
+    public static void cleanUp(){
+        driver.manage().deleteAllCookies();
+        driver.close();
+    }
+}
